e66c605fff940c4083185dce112a08a7157c237f
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2021-03-03 17:52:41 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const bodyParser = require("body-parser");
+
+
+const app = express()
+const port = process.env.PORT || 3003
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://userwozx:wozx1234567890@ewozx.neief.mongodb.net/registro';
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+
+mongoose.connect(uri, options).then(
+  () => { console.log("Conectado Exitodamente!");},
+  err => { console.log(err); }
+);
+
+var user = mongoose.model('usuarios', {
+        direccion: String,
+        registered: Boolean,
+        sponsor: String,
+        exist: Boolean,
+        ethereum: String,
+        eth: Boolean,
+        rango: Number,
+        recompensa: Boolean,
+        nivel: [Number],
+        balanceTrx: Number,
+        withdrawnTrx: Number,
+        investedWozx: Number,
+        withdrawnWozx: Number,
+        wozxPendig: Number,
+        p: Boolean
+    
+    });
+
+var usuariobuscado = 'david lopez lopez';
+
+var cosa2;
+
+app.get('/', async(req,res) => {
+
+    usuario = await user.find({ direccion: usuariobuscado }, function (err, docs) {});
+
+    console.log(usuario);
+
+    res.send(usuario[0]);
+
+});
+
+    
+
+
+
+app.post('/buscar', async(req,res) => {
+
+    let cuenta = req.body.cuenta;
+
+    usuario = await user.find({ direccion: cuenta }, function (err, docs) {});
+
+    console.log(usuario);
+
+    res.send(usuario[0]);
+
+});
+
+
+app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
